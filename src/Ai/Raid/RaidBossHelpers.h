c00bd48b3fafe1390fb7239b4695874c3f073074
Arkos <43470666+ArkosLUL@users.noreply.github.com>
2026-07-11 11:24:14 +0300

    Improve pet behavior during Lady Vashj Phase 2
    fix build

--- src/Ai/Raid/RaidBossHelpers.h
+++ src/Ai/Raid/RaidBossHelpers.h
@@ -22,5 +22,7 @@ Unit* GetFirstAliveUnitByEntry(
     PlayerbotAI* botAI, uint32 entry);
 Unit* GetNearestPlayerInRadius(Player* bot, float radius);
 bool IsBotInFrontalCone(Player* bot, Unit* source, float coneAngle, float range);
+void CommandPetAttack(PlayerbotAI* botAI, Unit* target);
+void StopPet(PlayerbotAI* botAI);
 
 #endif
